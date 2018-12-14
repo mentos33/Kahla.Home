cd9ff10f00019ddf3116049d578ff9f261974772
Anduin2017 <anduin@aiursoft.com>
2018-12-14 10:32:15 +0800

    Cache github get version.

--- Kahla.Home/Controllers/HomeController.cs
+++ Kahla.Home/Controllers/HomeController.cs
@@ -13,6 +13,9 @@ namespace Kahla.Home.Controllers
 {
     public class HomeController : Controller
     {
+        private static DateTime _cachedTime = DateTime.MinValue;
+        private static string _cachedVersion = "";
+
         private readonly VersionChecker _version;
         public HomeController(VersionChecker version)
         {
@@ -21,7 +24,17 @@ namespace Kahla.Home.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var latest = await _version.CheckKahla();
+            string latest = null;
+            if (DateTime.UtcNow < _cachedTime + new TimeSpan(0, 20, 0) && !string.IsNullOrEmpty(_cachedVersion))
+            {
+                latest = _cachedVersion;
+            }
+            else
+            {
+                latest = await _version.CheckKahla();
+                _cachedTime = DateTime.UtcNow;
+                _cachedVersion = latest;
+            }
             var model = new IndexViewModel
             {
                 LatestVersion = latest
